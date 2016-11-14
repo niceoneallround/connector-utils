1a957267e8cd6787c473b66fe9c143accbd14e09
niceoneallround <niceoneallround@gmail.com>
2016-11-14 11:01:01 -0800

    Add md utils for fetch metadata, check JWT, turn into a resource

---
+++ md-utils/lib/utils.js
@@ -0,0 +1,170 @@
+/*jslint node: true, vars: true */
+
+//
+// Provides utility routines for accessing and saving metadata using the
+// apigwRequestWrapper base level fetch/post metadata. But adds
+//  - performs JWt processing
+//  - error handling
+
+const apigwRequestWrapper = require('node-utils/apigwRequestWrapper/lib/apigwRequestWrapper');
+const assert = require('assert');
+const JWTUtils = require('jwt-utils/lib/jwtUtils').jwtUtils;
+const HttpStatus = require('http-status');
+const moment = require('moment');
+const MDUtils = require('metadata/lib/md').utils;
+const PNDataModel = require('data-models/lib/PNDataModel');
+const PN_T = PNDataModel.TYPE;
+const util = require('util');
+
+const loggingMD = {
+    ServiceType: 'md-utils',
+    FileName: 'md-utils/utils.js', };
+
+var promises = {};
+var callbacks = {};
+
+//---------------------
+// Fetch metadata
+//-------------------
+
+// added as worked on v2 as no idea why needed to pass in the req, and res for
+// create pipe, seemed bogus
+//
+promises.promiseFetchMetadata = function promiseFetchMetadata(serviceCtx, mdId, props) {
+  'use strict';
+  return new Promise(function (resolve, reject) {
+    callbacks.fetchMetadata(serviceCtx, mdId, props, function (err, mdResource) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(mdResource);
+      }
+    });
+  });
+};
+
+callbacks.fetchMetadata = function fetchMetadata(serviceCtx, mdId, props, callback) {
+  'use strict';
+
+  let mdIdParam = PNDataModel.ids.paramUtils.createMdParamFromMdId(mdId);
+  let domainIdParam = PNDataModel.ids.paramUtils.createParamFromDomain(serviceCtx.config.DOMAIN_NAME);
+
+  serviceCtx.logger.logJSON('info', { serviceType: serviceCtx.name, action: 'Fetch-Metadata',
+                          mdId: mdId, mdIdParam: mdIdParam, domainIdParam: domainIdParam,
+                        }, loggingMD);
+
+  //
+  // So can use the AWS APIGW do not pass the domainID as part of the path
+  //
+  //fetchProps.domainIdParam = domainIdParam;
+  let fetchProps = {};
+  fetchProps.mdIdParam = mdIdParam;
+  fetchProps.loggerMsgId = mdIdParam;
+  fetchProps.logMsgServiceName = serviceCtx.name;
+  fetchProps.logMsgPrefix = 'Fetch-Metadata';
+  fetchProps.logger = serviceCtx.logger;
+
+  apigwRequestWrapper.callbacks.fetchMetadataJWT(fetchProps, serviceCtx.config.API_GATEWAY_URL, function (err, response) {
+    if (err) {
+      serviceCtx.logger.logJSON('error', { serviceType: serviceCtx.name,
+                            action: 'Fetch-Metadata-ERROR-UNEXPECTED-ERROR',
+                            domainName: serviceCtx.config.DOMAIN_NAME,
+                            mdId: mdId,
+                            err: err, }, loggingMD);
+      return callback(err);
+    }
+
+    switch (response.statusCode) {
+      case HttpStatus.OK: {
+        //
+        // if configured to verify then verify
+        //
+        if (serviceCtx.config.VERIFY_JWT) {
+          try {
+            let verified = JWTUtils.newVerify(response.body, serviceCtx.config.crypto.jwt);
+            serviceCtx.logger.logJSON('info', { serviceType: serviceCtx.name,
+                                      action: 'Fetch-Metadata-JWT-VERIFY-PASSED',
+                                      mdId: mdId,
+                                      metadata: verified, }, loggingMD);
+          } catch (err) {
+            let error1 = PNDataModel.errors.createInvalidJWTError({
+                      id: PNDataModel.ids.createErrorId(serviceCtx.config.getHostname(), moment().unix()),
+                      type: PN_T.Metadata, jwtError: err, });
+
+            serviceCtx.logger.logJSON('error', { serviceType: serviceCtx.name,
+                                        action: 'Fetch-Metadata-ERROR-JWT-VERIFY', inputJWT: response.body,
+                                        error: error1,
+                                        mdId: mdId,
+                                        decoded: JWTUtils.decode(response.body,  { complete: true }),
+                                        jwtError: err, }, loggingMD);
+
+            return callback(error1, null);
+          }
+        }
+
+        let payload = JWTUtils.decode(response.body); // decode as may not have verified
+        let md = MDUtils.JWTPayload2Node(payload, serviceCtx.config.getHostname());
+
+        if (PNDataModel.errors.isError(md)) {
+          serviceCtx.logger.logJSON('error', { serviceType: serviceCtx.name,
+                            action: 'FETCH-Metadata-Payload-is-Error',
+                            mdId: mdId, error: md, }, loggingMD);
+          return callback(md, null);
+
+        } else {
+          serviceCtx.logger.logJSON('info', { serviceType: serviceCtx.name, action: 'FETCH-Metadata-OK',
+                            mdId: mdId,
+                            metadata: md,
+                            returnedStatusCode: response.statusCode, returnedHeaders: response.headers, }, loggingMD);
+          return callback(null, md);
+        }
+
+        break;
+      }
+
+      case HttpStatus.NOT_FOUND: {
+
+        if ((response) && (response.body)) {
+          // the metadata service can return an error in the body, if one
+          // then extract with decode and display
+          let decoded = JWTUtils.decode(response.body, serviceCtx.config.crypto.jwt);
+          serviceCtx.logger.logJSON('info', { serviceType: serviceCtx.name,
+                                action: 'Fetch-Metadata-INFO-NOT-FOUND',
+                                domainName: serviceCtx.config.DOMAIN_NAME,
+                                mdId: mdId,
+                                data: decoded, }, loggingMD);
+
+          // the error is in the graph so extract and return to caller
+          let error = JWTUtils.getPnGraph(decoded);
+          return callback(error, null);
+        } else {
+          serviceCtx.logger.logJSON('info', { serviceType: serviceCtx.name,
+                                action: 'Fetch-Metadata-INFO-NOT-FOUND-NO-RESPONSE-BODY-CHECK-API-GATEWAY-URL',
+                                domainName: serviceCtx.config.DOMAIN_NAME,
+                                mdId: mdId, }, loggingMD);
+
+          return callback(util.format('MD:%s NOT FOUND - check API-GATEWAY-URL', mdId), null);
+        }
+
+        break;
+      }
+
+      default: {
+        // unexpected response so for now just cause an exception
+        serviceCtx.logger.logJSON('error', { serviceType: serviceCtx.name,
+                              action: 'Fetch-Metadata-ERROR-UNEXPECTED-STATUS-CODE',
+                              domainName: serviceCtx.config.DOMAIN_NAME,
+                              mdId: mdId,
+                              statusCode: response.statusCode, }, loggingMD);
+
+        assert(false, util.format('Unexpected status fetching one metadata code:%s', response.statusCode));
+        return callback(util.format('Unexpected fetching one metadata status code:%s', response.statusCode), null);
+      }
+    }
+  });
+};
+
+module.exports = {
+  callbacks: callbacks,
+  promises: promises,
+};
---
+++ md-utils/test/testUtils.js
@@ -0,0 +1,139 @@
+/*jslint node: true, vars: true */
+const assert = require('assert');
+const conMDUtils = require('../lib/utils');
+const HttpStatus = require('http-status');
+const JWTUtils = require('jwt-utils/lib/jwtUtils').jwtUtils;
+const nock = require('nock');
+const MDUtils = require('metadata/lib/md').utils;
+const PNDataModel = require('data-models/lib/PNDataModel');
+const should = require('should');
+const testUtils = require('node-utils/testing-utils/lib/utils');
+const util = require('util');
+
+describe('Utils-Metadata', function () {
+  'use strict';
+
+  const API_GATEWAY_URL = 'http://fake.api.gateway';
+  let dummyServiceCtx;
+
+  function createPrivacyAlgorithmJWT(privacyAlgorithm) {
+    return JWTUtils.signMetadata(privacyAlgorithm, dummyServiceCtx.config.crypto.jwt, { subject: privacyAlgorithm['@id'] });
+  }
+
+  function createPrivacyAlgorithm() {
+    let yaml = {
+      id: '23',
+      type: 'privacyalgorithmv2',
+      description: 'A valid PA that shows all the mandatory fields, except this one',
+      privacy_step: [{
+        id: 'pstep-1',
+        description: 'a test privacy step',
+        node_type: 'connector',
+        privacy_action: [{
+          id: 'paction-1',
+          content_obfuscation_algorithm: 'A256GCM',
+          obfuscation_provider: 'http://ionicsecurity.com',
+          kms: 'http://md.pn.id.webshield.io/resource/com/acme#my-kms',
+          skip_orchestration: false,
+          schema:  {
+            $schema: 'http://experian.schema.webshield.io',
+            'http//json-schema.org/title': 'http://experian.schema.webshield.io/type#Subject',
+            'http://json-schema.org/type': 'object', },
+        },
+      ],
+      },
+      ],
+    };
+
+    // create a JSONLD node
+    let md = MDUtils.YAML2Node(yaml,
+                { hostname: dummyServiceCtx.config.getHostname(),
+                  domainName: dummyServiceCtx.config.DOMAIN_NAME,
+                  issuer: 'abc.com', creationTime: '282828', });
+
+    if (PNDataModel.errors.isError(md)) {
+      assert(false, util.format('failed to create privacy algorithm:%j', md));
+    }
+
+    return md;
+  }
+
+  before(function (done) {
+    let props = {};
+    props.name = 'test1';
+    testUtils.createDummyServiceCtx(props, function (ctx) {
+      dummyServiceCtx = ctx;
+      dummyServiceCtx.config = testUtils.getTestServiceConfig(
+          { port: '2325',
+            API_GATEWAY_URL: API_GATEWAY_URL,
+            DOMAIN_NAME: 'test.webshield.io', });
+      done();
+    });
+  });
+
+  describe('1 Valid Fetch', function () {
+
+    it('1.1 should return the JSON-LD metadata', function (done) {
+      let privacyAlgorithm = createPrivacyAlgorithm();
+      let mdId = privacyAlgorithm['@id'];
+
+      let msPathUrl = '/v1/metadata/privacy_algorithm___io___webshield___test--23';
+
+      nock(API_GATEWAY_URL)
+            .log(console.log)
+            .get(msPathUrl)
+            .reply(function () { // not used uri, requestBody) {
+              return [
+                HttpStatus.OK,
+                createPrivacyAlgorithmJWT(privacyAlgorithm),
+                { 'content-type': 'text/plain', },
+              ];
+            });
+
+      conMDUtils.promises.promiseFetchMetadata(dummyServiceCtx, mdId, {})
+        .then(
+          function (mdResource) {
+            mdResource.should.have.property('@id');
+            done();
+
+          }
+        )
+        .catch(function (err) {
+          assert(false, util.format('1.1 Should not have caught an error:%s', err));
+          done();
+        });
+    }); //it 1.1
+  }); // describe 1
+
+  describe('2 InValid Fetch - no nock match so not found', function () {
+
+    it('2.1 should return an error', function (done) {
+      let privacyAlgorithm = createPrivacyAlgorithm();
+      let mdId = privacyAlgorithm['@id'];
+      let msPathUrl = 'not found';
+
+      nock(API_GATEWAY_URL)
+            .log(console.log)
+            .get(msPathUrl)
+            .reply(function () { // not used uri, requestBody) {
+              return [
+                HttpStatus.OK,
+                'should-not-get-here',
+                { 'content-type': 'text/plain', },
+              ];
+            });
+
+      conMDUtils.promises.promiseFetchMetadata(dummyServiceCtx, mdId, {})
+        .then(
+          function () {
+            assert(false, 'should not get here as an error');
+            done();
+          }
+        )
+        .catch(function () {
+          assert(true, 'should-get-here-as-an-error');
+          done();
+        });
+    }); //it 2.1
+  }); // describe 1
+}); // describe
