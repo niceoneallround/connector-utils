5dffb303f17f0879e2ba5f0f8f583b23d6ab4de4
niceoneallround <niceoneallround@gmail.com>
2016-11-17 16:31:41 -0800

    Add more debug

--- pipe-utils/lib/utils.js
+++ pipe-utils/lib/utils.js
@@ -167,11 +167,16 @@ callbacks.createPrivacyPipe = function createPrivacyPipe(serviceCtx, requestId, 
       case HttpStatus.BAD_REQUEST: {
         // bad request is returned as an application/json
         if (response.headers['content-type'] === 'application/json') {
+          console.log('*****----BODY IS', response.body);
           console.log('*******---- TYPEOF BODY IS:%s', typeof response.body);
           let err = JSON.parse(response.body);
+          console.log('*****----JSON.parse BODY IS', err);
           console.log('*******---- TYPEOF err IS:%s', typeof err);
-          let err2 = JSON.parse(response.body);
-          console.log('*******---- TYPEOF err2 IS:%s', typeof err2);
+          if (typeof err === 'string') {
+            let err2 = JSON.parse(err);
+            console.log('*******---- STILL A STRING TYPEOF err2 IS:%s', typeof err2);
+          }
+
           serviceCtx.logger.logJSON('info', { serviceType: serviceCtx.name,
                                     action: 'Create-PRIVACY-PIPE-FAILED-BAD-REQUEST',
                                     requestId: requestId,
