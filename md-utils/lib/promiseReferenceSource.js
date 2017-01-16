4d4c10c64e74e91373043a3414149ebc9ea34ef3
niceoneallround <niceoneallround@gmail.com>
2017-01-15 17:12:45 -0800

    Add utility to fetch the reference source

---
+++ md-utils/lib/promiseReferenceSource.js
@@ -0,0 +1,57 @@
+/*jslint node: true, vars: true */
+
+/*
+
+  Fetches and Validate a REFERENCE SOURCE if Ok returns a structure
+   { rs: the rs
+   }
+
+  otherwise throws a PN Type Error
+
+*/
+
+const assert = require('assert');
+const MServiceUtils = require('./utils');
+
+function execute(serviceCtx, rsId, msgId, actionMsg) {
+  'use strict';
+  assert(serviceCtx, 'referenceSource - serviceCtx param missing');
+  assert(rsId, 'referenceSource - rsId param missing');
+  assert(msgId, 'referenceSource -  msgId param missing');
+  assert(actionMsg, 'referenceSource - actionMsg param missing');
+
+  const loggingMD = {
+          ServiceType: serviceCtx.name,
+          FileName: 'connector-utils/promiseReferenceSource.js', };
+
+  let result = {};
+
+  serviceCtx.logger.logJSON('info', { serviceType: serviceCtx.name, action: actionMsg + '-Fetching-Reference-Source',
+                                    referenceSourceId: rsId,
+                                    msgId: msgId, }, loggingMD);
+
+  return MServiceUtils.promises.fetchMetadata(serviceCtx, rsId, {})
+  .then(
+    function (referenceSource) {
+      result.rs = referenceSource;
+      serviceCtx.logger.logJSON('info', { serviceType: serviceCtx.name, action: actionMsg + '-Fetched-Reference-Source',
+                                          referenceSourceId: rsId,
+                                          msgId: msgId,
+                                          metadata: referenceSource, }, loggingMD);
+      return result;
+    },
+
+    function (err) {
+      serviceCtx.logger.logJSON('error', { serviceType: serviceCtx.name,
+                                          action: actionMsg + '-ERROR-Fetching-Reference-Source',
+                                          msgId: msgId,
+                                          referenceSourceId: rsId,
+                                          error: err, }, loggingMD);
+      throw err; // let final catch handle
+    }
+  );
+}
+
+module.exports = {
+  execute: execute,
+};
---
+++ md-utils/test/testPromiseReferenceSource.js
@@ -0,0 +1,77 @@
+/*jslint node: true, vars: true */
+const HttpStatus = require('http-status');
+const assert = require('assert');
+const JWTUtils = require('jwt-utils/lib/jwtUtils').jwtUtils;
+const nock = require('nock');
+const RSCanons = require('metadata/lib/ReferenceSource').canons;
+const promiseReferenceSource = require('../lib/promiseReferenceSource');
+const should = require('should');
+const testUtils = require('node-utils/testing-utils/lib/utils');
+const util = require('util');
+
+describe('Promise Reference Source Tests', function () {
+  'use strict';
+
+  let dummyServiceCtx;
+  const API_GATEWAY_URL = 'http://fake.api.gateway';
+
+  before(function (done) {
+    let props = {};
+    props.name = 'test-service';
+    testUtils.createDummyServiceCtx(props, function (ctx) {
+      dummyServiceCtx = ctx;
+      dummyServiceCtx.config = testUtils.getTestServiceConfig(
+          { port: '2325',
+            API_GATEWAY_URL: API_GATEWAY_URL,
+            DOMAIN_NAME: 'test.webshield.io', });
+      done();
+    });
+  });
+
+  it('1.1 should post a request to the metadata service and return a structure with results', function () {
+
+    //
+    // Nock out call to get the Reference Source
+    let referenceSource = createReferenceSource();
+    let pathUrl = '/v1/metadata/reference_source___io___webshield___test--test-rs-1';
+    nock(API_GATEWAY_URL)
+          .log(console.log)
+          .get(pathUrl)
+          .reply(function () { // not used uri, requestBody) {
+            return [
+              HttpStatus.OK,
+              createMDJWT(referenceSource),
+              { 'content-type': 'text/plain', },
+            ];
+          });
+
+    return promiseReferenceSource.execute(dummyServiceCtx, referenceSource['@id'], 'msg-1', 'actionMsg-1')
+      .then(function (result) {
+        result.should.have.property('rs');
+      },
+
+      function (err) {
+        assert(false, util.format('Unexpected error:%j', err));
+        throw err;
+      });
+
+  });
+
+  //--------------
+  // Test Helpers
+  //--------------
+
+  function createMDJWT(md) {
+    return JWTUtils.signMetadata(md, dummyServiceCtx.config.crypto.jwt, { subject: md['@id'] });
+  }
+
+  // Canon
+  function createReferenceSource() {
+    let props =   { hostname: dummyServiceCtx.config.getHostname(),
+        domainName: dummyServiceCtx.config.DOMAIN_NAME,
+        issuer: 'abc.com', creationTime: '282828', };
+
+    return RSCanons.createTestReferenceSource(props);
+  }
+
+}); // describe
